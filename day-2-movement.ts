a2a97f391d7c33c78bef7f823ae37c868f0b9859
Jonathan Dahan <git@jonathan.is>
2021-12-03 11:55:49 -0500

    day 2 part 2

--- day-2-movement.ts
+++ day-2-movement.ts
@@ -6,14 +6,21 @@ console.log(getPosition(input));
 
 function getPosition(input: string) {
   let [horizontal, depth] = [0, 0];
+  let aim = 0;
 
   input
     .split("\n")
     .map((line) => line.split(" "))
     .forEach(([direction, magnitude]) => {
-      if (direction === "forward") horizontal += parseInt(magnitude);
-      if (direction === "up") depth -= parseInt(magnitude);
-      if (direction === "down") depth += parseInt(magnitude);
+      const units = parseInt(magnitude);
+
+      if (direction === "up") aim -= units;
+      if (direction === "down") aim += units;
+
+      if (direction === "forward") {
+        horizontal += units;
+        depth += aim * units;
+      }
     });
 
   return horizontal * depth;
