3d857c35d5fdc5f6b3501013513167c29fcecc93
Jonathan Dahan <git@jonathan.is>
2021-12-04 12:03:36 -0500

    cleanup

--- day-3-power.ts
+++ day-3-power.ts
@@ -1,32 +1,31 @@
-// deno run --allow-read=input day-3-sonar-sweep.ts
+// deno run --allow-read=input day-3-power.ts
 
 const path = Deno.args[0] ?? "input";
-const input = (await Deno.readTextFile(path)).split('\n');
+const input = (await Deno.readTextFile(path)).split("\n");
 console.log(lifeSupportRating(input));
 
 function lifeSupportRating(input: string[]) {
-  const oxygenRating = rating(input, true)
-  const scrubberRating = rating(input, false)
-  return oxygenRating * scrubberRating
+  const oxygenRating = rating(input, true);
+  const scrubberRating = rating(input, false);
+  return oxygenRating * scrubberRating;
 }
 
-function rating(input: string[], invert: boolean) {
-  let rating: string[] = []
+function rating(input: string[], mostCommon: boolean) {
+  let index = 0;
 
-  while(input.length > 1) {
+  while (input.length > 1) {
     const digitSum = input
-      .map((line) => line[rating.length] === '0' ? -1 : 1)
-      .reduce((prev, curr) => prev + curr, 0)
-    
-    const digit = invert 
-      ? (digitSum >= 0 ? '0' : '1') // if most common is 1, take 0
-      : (digitSum >= 0 ? '1' : '0') // if most common is 1, take 1
-    
-    rating.push(digit)
+      .map((line) => line[index] === "0" ? -1 : 1)
+      .reduce((prev, curr) => prev + curr, 0);
 
-    input = input.filter((line) => line[rating.length-1] === digit)
+    const digit = mostCommon
+      ? (digitSum >= 0 ? "0" : "1")
+      : (digitSum >= 0 ? "1" : "0");
+
+    input = input.filter((line) => line[index] === digit);
+
+    index++;
   }
 
-  const ratingValue = parseInt(input[0], 2)
-  return ratingValue
+  return parseInt(input[0], 2);
 }
